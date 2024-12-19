e585bde63f855b9cd63ba1b9bcb1d5be352e7510
gargDhruv12 <dhruvgarg729729@gmail.com>
2024-12-20 00:35:49 +0530

    upto vid-45 add data to database

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,28 @@ const express = require('express')
 connectToMongo();
 const app = express()
 const port = 3000
+app.use(express.json())
+// Available routes
+// app.get('/', (req, res) => {
+//   res.send('Hello Dhruv!')
+// })
+// app.get('/api/v1/login', (req, res) => {
+//   res.send('Hello login!')
+// })
+// app.get('/api/v1/signUp', (req, res) => {
+//   res.send('Hello sign up!')
+// })
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
-app.get('/', (req, res) => {
-  res.send('Hello Dhruv!')
-})
-
+app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+      console.error('Bad JSON');
+      return res.status(400).send({ error: 'Invalid JSON' });
+    }
+    next();
+  });
+  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
