3b35cdf42a6ec4bf3d22a85f2d13d804068548d8
gargDhruv12 <dhruvgarg729729@gmail.com>
2024-12-19 17:41:59 +0530

    Upto vid-43 started backend

---
+++ backend/db.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://localhost:27017/"
+
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log("Connected to MongoDB successfully");
+    } catch (error) {
+        console.error("Failed to connect to MongoDB", error);
+        process.exit(1); // Exit the application if the connection fails
+    }
+};
+
+module.exports = connectToMongo;
+
+
+
+// const mongoose = require('mongoose');
+// const mongoURI = "mongodb://localhost:27017/"
+// const connectToMongo = ()=>{
+//     mongoose.connect(mongoURI,()=>{
+//         console.log("Connected to mongo succesfully")
+//     })
+// }
+// module.exports = connectToMongo;
